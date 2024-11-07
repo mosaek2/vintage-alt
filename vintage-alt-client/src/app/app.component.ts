d2daf23a43cf42ee1c5fa48fb51eda1012b38055
mosaek2 <mosaek2@gmail.com>
2024-11-07 09:53:32 +0900

    home, login, join routing

--- vintage-alt-client/src/app/app.component.ts
+++ vintage-alt-client/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'vintage-alt-client';
+  constructor(private router: Router) { }
+
+  navigateTo(page: string): void {
+    this.router.navigate([page]);
+  }
 }
--- vintage-alt-client/src/app/app.routes.ts
+++ vintage-alt-client/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { JoinComponent } from './pages/join/join.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'join', component: JoinComponent },
+  { path: 'login', component: LoginComponent }
 ];
